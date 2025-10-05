bceab0dc982b2ef5781adbeda1cd46ebbf9cac76
nmtu91174 <nmtu91174@gmail.com>
2025-10-05 21:12:21 +0700

    edit table

--- js/compare.js
+++ js/compare.js
@@ -39,8 +39,8 @@ function compareProducts() {
       </tr>
       <tr>
         <td>Image</td>
-        <td><img src="${product1.thumbnail}" alt="${product1.title}" width="120"></td>
-        <td><img src="${product2.thumbnail}" alt="${product2.title}" width="120"></td>
+        <td><img src="${product1.thumbnail}" alt="${product1.title}" width="150"></td>
+        <td><img src="${product2.thumbnail}" alt="${product2.title}" width="150"></td>
       </tr>
       <tr>
         <td>Price</td>
